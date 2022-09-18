ab4cd3eda5a88833e27db05cef708ba06217f43b
yepengding <dingyepeng@gmail.com>
2022-09-18 20:27:30 +0200

    Add resolving VC documents.

---
+++ src/util/ContextUtil.ts
@@ -0,0 +1,19 @@
+/**
+ * Context Util
+ *
+ * @author Yepeng Ding
+ */
+export class ContextUtil {
+
+    /**
+     * Get default context of VC
+     *
+     */
+    public static defaultContextOfVC(): [string, string] {
+        return [
+            "https://www.w3.org/2018/credentials/v1",
+            "https://www.w3.org/2018/credentials/examples/v1"
+        ];
+    }
+
+}
