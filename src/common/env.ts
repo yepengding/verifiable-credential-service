f5bcfaafcb1c8563620a01347269220dd0c23ef2
yepengding <dingyepeng@gmail.com>
2022-10-12 12:33:32 +0200

    Fix Public Key DTO.

--- src/common/env.ts
+++ src/common/env.ts
@@ -20,7 +20,7 @@ export const env = {
         endpoint: `${process.env.APP_DOMAIN || 'http://localhost'}:${process.env.APP_PORT || 9000}`
     },
     log: {
-        dir: process.env.LOG_DIR || 'log',
+        dir: process.env.LOG_DIR || 'logs',
         format: process.env.LOG_FORMAT || 'common'
     },
     db: {
